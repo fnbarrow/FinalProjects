e4b0e3154c8d47e5875d297b81f89e481c9aa7cb
fnbarrow <32984794+fnbarrow@users.noreply.github.com>
2017-11-18 12:26:17 -0600

    Finish Counting Letters Project

---
+++ CountingLetters.py
@@ -0,0 +1,16 @@
+'''
+Created on Nov 18, 2017
+
+@author: ITAUser
+'''
+#open the text file
+#read the file 
+#split the file
+#count the letters
+#print result
+
+def CountingLetters(filename, mychar):
+   f = open(filename, 'r')
+   count = 0;
+   f.read('r')
+   
