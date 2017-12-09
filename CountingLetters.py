47f394311b293a0372bd9181f8acad433501052d
fnbarrow <32984794+fnbarrow@users.noreply.github.com>
2017-12-08 23:32:10 -0600

    Counting Letters
    Final project

--- CountingLetters.py
+++ CountingLetters.py
@@ -13,4 +13,17 @@ def CountingLetters(filename, mychar):
    f = open(filename, 'r')
    count = 0;
    f.read('r')
-   
+   run = True
+   while run:
+       letter = f.read(1)
+       letter = letter.lower()
+       if letter == "":
+        break 
+    else: 
+        if letter == mychar:
+            count = count + 1
+    print(count)
+    
+CountingLetters(filename="consitution.txt", mychar="a")
+
+    
